2472641438fcc04bb50a9856141e4c4554f6f94e
Alina0609 <grin4enko.alina@yandex.ru>
2023-04-19 20:18:17 +0300

    added a structure for storing coordinates and creating the main window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtWidgets>
+#include <QPainter>
+#include <QtMath>
+
+struct qp //структура для хранения координаты при рисовании
+{
+    QPoint pos;
+    int fl;
+};
+
+//главное окно
+class mainw: public QFrame
+{
+public:
+    explicit mainw();
+    void paintEvent(QPaintEvent *event);
+    QPoint pos; //координаты точки для рисования
+    void mouseMoveEvent(QMouseEvent *event);
+    QVector<qp> vcp; //массив (вектор) для хранения рисунка
+    void mousePressEvent(QMouseEvent *);
+    void mouseReleaseEvent(QMouseEvent *);
+    int f;
+};
+
+
 
 int main(int argc, char *argv[])
 {
